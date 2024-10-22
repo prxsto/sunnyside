6fa2a3473ffdf0086b8ce9ac6b473d7e7a68239a
prxsto <15711032+prxsto@users.noreply.github.com>
2024-10-22 10:24:41 -0700

    scaffold cobra app

---
+++ cmd/root.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"context"
+
+	"github.com/spf13/cobra"
+)
+
+// entry function
+func Execute() error {
+	rootCmd := &cobra.Command{
+		Version: "v0.0.1",
+		Use:     "sunnyside",
+		Long:    "sunnyside is a TUI-based recipe repository & meal planner",
+		Example: "sunnyside",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return nil
+		},
+	}
+
+	return rootCmd.ExecuteContext(context.Background())
+}
